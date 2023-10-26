add306d2493659e3a91730d96f431a725a26c841
Marcus Vinicius <mvcoosta@gmail.com>
2023-10-26 15:21:22 -0400

    implement lambda function

---
+++ python/README.md
@@ -0,0 +1,8 @@
+Acesse o seguinte github https://github.com/jkehler/awslambda-psycopg2, entre na pasta with_ssl_support e baixe o psycog compatível com a versão do python a ser utilizada.
+Renomeie a pasta para "pyscopg2".
+
+Instale a dependência do jwt com o comando.
+pip install PyJWT -t .
+
+Crie uma pasta zip com o código python e as suas depedências.
+zip -r lambda.zip
---
+++ python/lambda.py
@@ -0,0 +1,88 @@
+import psycopg2
+import json
+import boto3
+import jwt
+from datetime import datetime, timedelta
+
+# Use this code snippet in your app.
+# If you need more information about configurations
+# or implementing the sample code, visit the AWS docs:
+# https://aws.amazon.com/developer/language/python/
+
+secret_name = "fiap-db"
+region_name = "us-east-1"
+
+# Create a Secrets Manager client
+session = boto3.session.Session()
+client = session.client(service_name="secretsmanager", region_name=region_name)
+
+get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+
+# Decrypts secret using the associated KMS key.
+credential = json.loads(get_secret_value_response["SecretString"])
+credential["db"] = "fiap"
+
+connection = psycopg2.connect(
+    user=credential["username"],
+    password=credential["password"],
+    host=credential["host"],
+    database=credential["db"],
+)
+
+
+def generate_jwt_token(id=None, name=None, cpf=None):
+    issued_at = datetime.utcnow()
+    expiration_time = issued_at + timedelta(days=30)
+
+    payload = {
+        "sub": id,
+        "name": name,
+        "cpf": cpf,
+        "iat": issued_at.timestamp(),
+        "exp": expiration_time.timestamp(),
+    }
+
+    secret_key = "5nOMyyNNXhb0dVBWwG6dFCxzIatOBIaWhNELxCfbOkeeiBrElSWXILGDsvHQqlFx"
+    algorithm = "HS256"
+
+    token = jwt.encode(payload, secret_key, algorithm=algorithm)
+    return token
+
+def generate_response(token):
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps({"token": token})
+    }
+
+def lambda_handler(event, context):
+    body = event.get("body")
+    if not body:
+        token = generate_jwt_token()
+        return generate_response(token)
+
+    try:
+        decodedEvent = json.loads(body)
+    except:
+        decodedEvent = body
+    cpf = decodedEvent.get("cpf")
+    if not cpf:
+        token = generate_jwt_token()
+        return generate_response(token)
+
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM cliente WHERE cpf = %s"
+
+    cursor.execute(query, (cpf,))
+    results = cursor.fetchone()
+    cursor.close()
+    connection.commit()
+
+    if not results:
+        token = generate_jwt_token(cpf=cpf)
+        return generate_response(token)
+
+    token = generate_jwt_token(results[0], results[1], cpf)
+    return generate_response(token)
+
